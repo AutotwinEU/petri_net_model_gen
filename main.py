ba7d7449ecf6158815ae24b8466c3854f660aa9d
weicheng22 <wei.cheng.me@outlook.com>
2024-09-10 15:00:28 +0200

    update config.json

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import os
 import autotwin_gmglib as gmg
 # import src.autotwin_pnglib as png
 import autotwin_pnglib as png
 
 if __name__ == "__main__":
     # load config
-    config_path = 'data/croma-case/config.json'
+    config_path = 'config/croma-config.json'
     config = gmg.load_config(config_path)
+
+    # set path
+    if not os.path.exists(config['work_path']):
+        os.makedirs(config['work_path'])
+    config['path']['recons_state'] = os.path.join(config['work_path'], config['path']['recons_state'])
+    config['path']['input_data'] = os.path.join(config['work_path'], config['path']['input_data'])
+    config['path']['model'] = os.path.join(config['work_path'], config['path']['model'])
 
     # extract log from skg, reconstruct state, and generate input data for Petri net generation
     png.reconstruct_state(config)
